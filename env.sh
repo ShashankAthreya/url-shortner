4e8d459aef35bb82ab1fdec3b16eed137520e330
Shashank Athreya <athreya.shashank@protonmail.com>
2022-04-11 10:27:05 +0200

    Added bash files for easy installation

---
+++ env.sh
@@ -0,0 +1,5 @@
+sudo apt-get update;
+sudo apt-get -y upgrade;
+sudo apt-get install -y python3-pip;
+sudo apt-get install -y python3-venv;
+python3 -m venv env;
---
+++ start.sh
@@ -0,0 +1,4 @@
+pip install flask hashids;
+export FLASK_APP=app;
+export FLASK_ENV=development;
+flask run --host=0.0.0.0;
